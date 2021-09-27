6560e64d4e28473bb243684fe300f64158e4fd50
leolpaz <leonardolpaz95@gmail.com>
2021-09-27 14:04:09 -0300

    Prepare for github pages

--- src/complete.js
+++
@@ -1,9 +0,0 @@
-export function checked(id, arr) {
-  arr[id].completed = true;
-  localStorage.setItem('taskArray', JSON.stringify(arr));
-}
-
-export function unchecked(id, arr) {
-  arr[id].completed = false;
-  localStorage.setItem('taskArray', JSON.stringify(arr));
-}
